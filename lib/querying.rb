f9e634bfc9a3c60f03ff60eba117b56f507ec68f
Jeff Swisher <jefftswisher@gmail.com>
2019-12-15 02:56:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,11 +10,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT MAX(characters.species) from characters; "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT "
 end
 
 def select_series_title_with_most_human_characters
